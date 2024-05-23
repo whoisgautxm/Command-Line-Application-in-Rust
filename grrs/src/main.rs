5b8cc9f0b2e34d3e4a0a3f00830c65ce3054cd0e
Gautam Kumar <129857715+whoisgautxm@users.noreply.github.com>
2024-05-24 02:39:25 +0530

    Basic CLI implemented with Testing

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Ok, Result};
 use clap::Parser;
-
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
@@ -10,17 +9,28 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result).unwrap();
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
+    find_matches(&content, &args.pattern, &mut std::io::stdout())?;
+
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write)-> Result<()> {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
         }
     }
-
     Ok(())
 }
